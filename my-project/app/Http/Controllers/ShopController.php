49b71eda12739c560c7f0265766cbacc6b7d9227
sca_tech <sca-license@sca.ac.jp>
2024-12-05 15:47:23 +0900

    テーブルにデータ追加機能実装

--- my-project/app/Http/Controllers/ShopController.php
+++ my-project/app/Http/Controllers/ShopController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function create() {
+    public function create(Request $request) {
+        $email = $request->input('email');
+
+        DB::table('shops')->insert([
+            'name' => 'test name',
+            'email' => $email,
+            'phone_number' => '0000000000'
+        ]);
+
         return "created shop";
     }
 }
--- my-project/resources/views/register.blade.php
+++ my-project/resources/views/register.blade.php
@@ -6,6 +6,7 @@
 <body>
     <form action="/create" method="POST">
         @csrf
+        <input type="text" name="email"/>
         <button type="submit">登録</button>
     </form>
 </body>
